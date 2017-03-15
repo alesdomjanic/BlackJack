b8756813d9ca5c847088c33b95104d8fe2d65514
alesdomjanic <alesdomjanic1@gmail.com>
2017-03-15 09:55:13 -0400

    "Commit11"

--- src/blackjack/BlackJack.java
+++ src/blackjack/BlackJack.java
@@ -1,12 +1,34 @@
 
 package blackjack;
 
+import java.util.Random;
+
 
 public class BlackJack {
 
     
     public static void main(String[] args) {
         
+        
+    }
+    
+    
+    public class Card{
+        private int rand;
+        private int value;
+        private int suit;
+        
+        public Card(){
+            
+            Random random=new Random();
+            int number=random.nextInt(52);
+            this.rand=number;
+            
+            if(this.rand >=0 && this.rand <3){
+                this.value=2;
+            }
+            
+        }
         
     }
     
