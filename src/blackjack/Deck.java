78fc283465b2328cbcd1322ca6e73dc28bc90644
alesdomjanic <alesdomjanic1@gmail.com>
2017-03-16 12:47:40 -0400

    "Commit3"

---
+++ src/blackjack/Deck.java
@@ -0,0 +1,52 @@
+package blackjack;
+
+import java.util.ArrayList;
+
+public class Deck {
+
+    private ArrayList<Card> deck;
+    private Card rand;
+
+    public Deck() {
+
+        deck = new ArrayList<Card>();
+        barva(Barva.CLUBS);
+        barva(Barva.DIAMONDS);
+        barva(Barva.HEARTS);
+        barva(Barva.SPADES);
+        
+    }
+        
+    private void barva(Barva b) {
+        deck.add(new Card(b, Rank.TWO));
+        deck.add(new Card(b, Rank.THREE));
+        deck.add(new Card(b, Rank.FOUR));
+        deck.add(new Card(b, Rank.FIVE));
+        deck.add(new Card(b, Rank.SIX));
+        deck.add(new Card(b, Rank.SEVEN));
+        deck.add(new Card(b, Rank.EIGHT));
+        deck.add(new Card(b, Rank.NINE));
+        deck.add(new Card(b, Rank.TEN));
+        deck.add(new Card(b, Rank.JACK));
+        deck.add(new Card(b, Rank.QUEEN));
+        deck.add(new Card(b, Rank.KING));
+        deck.add(new Card(b, Rank.AS));
+    }
+    
+    public void izpis(){
+        for(int i=0;i<deck.size();i++){
+            deck.get(i).izpis();
+        }
+    }
+    
+    public Card random(){
+        int x=52;
+        for(int i=1;i<52;i--){
+            int rand=(int)(Math.random()*x+1);
+            deck.remove(rand);
+        }
+        return rand;
+    }
+
+  }
+
