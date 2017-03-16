a4477a6392b83f09b316584c843d437ff3ea547e
alesdomjanic <alesdomjanic1@gmail.com>
2017-03-16 09:49:07 -0400

    "Commit3"

--- src/blackjack/Card.java
+++ src/blackjack/Card.java
@@ -5,8 +5,6 @@ public class Card {
     
     private Barva barva;
     private Rank rank;
-    private int Value;
-    private String Rank;
     
     public Card(Barva barva,Rank rank){
         this.barva = barva;
@@ -14,68 +12,29 @@ public class Card {
         
     }
     
-    public int Value(){
+    public void getValue(){
         
-        int PlayerCard = 0;
-        
-        switch(PlayerCard){
-            case 2:
-                Rank = "TWO";
-                Value = 2;
-                break;
-            case 3:
-                Rank = "THREE";
-                Value = 3;
-                break;
-            case 4:
-                Rank = "FOUR";
-                Value = 4;
-                break;
-            case 5:
-                Rank = "FIVE";
-                Value = 5;
-                break;
-            case 6:
-                Rank = "SIX";
-                Value = 6;
-                break;
-            case 7:
-                Rank = "SEVEN";
-                Value = 7;
-                break;
-            case 8:
-                Rank = "EIGHT";
-                Value = 8;
-                break;
-            case 9:
-                Rank = "NINE";
-                Value = 9;
-                break;
-            case 10:
-                Rank= "TEN";
-                Value = 10;
-                break;
-            case 11:
-                Rank= "JACK";
-                Value = 10;
-                break;
-            case 12:
-                Rank= "QUEEN";
-                Value = 10;
-                break;   
-            case 13:
-                Rank= "KING";
-                Value = 10;
-                break;
-            case 14:
-                Rank= "AS";
-                Value = 11;
-                break;  
+        switch(rank){
+        case TWO: System.out.println(2);
+        case THREE: System.out.println(3);
+        case FOUR: System.out.println(4);
+        case FIVE: System.out.println(5);
+        case SIX: System.out.println(6);
+        case SEVEN: System.out.println(7);
+        case EIGHT: System.out.println(8);
+        case NINE: System.out.println(9);
+        case TEN: System.out.println(10);
+        case JACK: System.out.println(10);
+        case QUEEN: System.out.println(10);
+        case KING: System.out.println(10);
+        case AS: System.out.println(11);
         }
         
-        return PlayerCard;
+    }
         
     }
     
     
-}
+    
+    
+
