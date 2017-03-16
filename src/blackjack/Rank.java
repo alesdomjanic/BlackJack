80d575acb018b8227ef486b80276cb8723179cdd
alesdomjanic <alesdomjanic1@gmail.com>
2017-03-16 14:23:05 -0400

    "Commit3"

---
+++ src/blackjack/Rank.java
@@ -0,0 +1,20 @@
+package blackjack;
+
+public enum Rank {
+
+    TWO,
+    THREE,
+    FOUR,
+    FIVE,
+    SIX,
+    SEVEN,
+    EIGHT,
+    NINE,
+    TEN,
+    JACK,
+    QUEEN,
+    KING,
+    AS;
+
+
+}
